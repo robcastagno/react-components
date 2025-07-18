e6f1f2d60387ddc3118f7c0062edfd0aeaa5b2d0
robcastagno <castagnorobert@gmail.com>
2025-07-17 22:53:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Components
+- Project is for testing and building small UI components to be used across other projects
+
+# Current Components
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { type ReactElement } from 'react'
+
+
+function App(): ReactElement {
+  return (
+    <>
+      <h1 className="bg-indigo-600 text-9xl">Hello, World!</h1>
+    </>
+  )
+}
+
+export default App
